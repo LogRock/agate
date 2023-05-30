99d2eb483f20f7342329a6cb88206229b4ea4c3a
gutembergsa <gutemberg.sa@logrock.com>
2023-05-30 17:28:27 -0300

    feat: initial commit for emails repo, putting welcome mail, inspections mail and youtube mail files

---
+++ inspections/mail-data.sql
@@ -0,0 +1,38 @@
+{   
+    "date": "May 2023",
+    "inspections": 3,
+    "violations": 5,
+    "oos": 2,
+    "details": [
+        {
+            "inspection_id": "#142801",
+            "total_violations": 3,
+            "date": "2022-01-03",
+            "unit_license": "124MG3",
+            "report_state": "IL",
+            "violations": [
+                {
+                    "viol_code": "3922SLLS4",
+                    "section_desc": "State/Local Laws - Speeding 15 or more miles per hour over the speed limit.",
+                    "basic_desc": "Unsafe Driving",
+                    "oos_indicator": "Y"
+                }
+            ] 
+        },
+        {
+            "inspection_id": "#142802",
+            "total_violations": 3,
+            "date": "2022-01-03",
+            "unit_license": "124MG3",
+            "report_state": "IL",
+            "violations": [
+                {
+                    "viol_code": "3922SLLS4",
+                    "section_desc": "State/Local Laws - Speeding 15 or more miles per hour over the speed limit.",
+                    "basic_desc": "Unsafe Driving",
+                    "oos_indicator": "Y"
+                }
+            ] 
+        }
+    ]
+}
